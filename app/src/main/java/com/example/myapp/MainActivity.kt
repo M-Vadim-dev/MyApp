af938ed1caf277afa04ae8965129651de87f4e46
M-Vadim-dev <railer@bk.ru>
2024-08-31 02:49:37 +0300

    RA-03-create-layout

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.myapp
 
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.service.voice.VoiceInteractionSession.ActivityId
+import android.view.LayoutInflater
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.example.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+//        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        binding.btnCategory.text = "Категории"
+        binding.btnCategory.setTextColor(Color.WHITE)
+        binding.btnCategory.isVisible = true
+        binding.btnFavourites.setTextColor(Color.WHITE)
+        binding.btnFavourites.isVisible = true
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
