0fc042ce22609876a43390f185449f6e535f0fb2
M-Vadim-dev <railer@bk.ru>
2024-09-02 23:33:45 +0300

    RA-04-create-fragment

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.myapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.replace
 import com.example.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val categoryFragment = CategoriesListFragment()
+        fragmentTransaction.add(binding.mainContainer.id, categoryFragment).commit()
 
     }
 }
