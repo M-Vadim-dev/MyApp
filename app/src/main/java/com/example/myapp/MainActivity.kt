47d50ac9e077c7af1a6c719b82f94e757bff312f
M-Vadim-dev <railer@bk.ru>
2024-09-02 21:49:31 +0300

    RA-03-correct-mistakes

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,16 +1,7 @@
 package com.example.myapp
 
-import android.app.Activity
-import android.graphics.Color
 import android.os.Bundle
-import android.service.voice.VoiceInteractionSession.ActivityId
-import android.view.LayoutInflater
-import android.view.inputmethod.InputBinding
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.isVisible
 import com.example.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
