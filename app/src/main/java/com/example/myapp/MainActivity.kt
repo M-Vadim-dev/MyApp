615ca50a8450a045858ab4ef04ceba612c8add9d
M-Vadim-dev <railer@bk.ru>
2024-11-30 23:59:36 +0300

    RA-28-implement-jetpack-navigation

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -2,49 +2,23 @@ package com.example.myapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import com.example.myapp.databinding.ActivityMainBinding
-import com.example.myapp.ui.recipes.favorites.FavoritesFragment
-import com.example.myapp.ui.categories.CategoriesListFragment
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<CategoriesListFragment>(R.id.mainContainer)
-            }
-        }
-
         binding.btnFavourites.setOnClickListener {
-            supportFragmentManager.commit {
-                replaceFragment<FavoritesFragment>()
-            }
+            findNavController(R.id.nav_host_fragment).navigate(R.id.favoritesFragment)
         }
 
         binding.btnCategory.setOnClickListener {
-            supportFragmentManager.commit {
-                replaceFragment<CategoriesListFragment>()
-            }
-        }
-
-    }
-
-    private inline fun <reified T : Fragment> replaceFragment() {
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            replace<T>(R.id.mainContainer)
+            findNavController(R.id.nav_host_fragment).navigate(R.id.categoriesListFragment)
         }
     }
 }
