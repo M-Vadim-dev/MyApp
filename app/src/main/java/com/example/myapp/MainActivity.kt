7e70c616beddecb79878e6fdac1bdb7f80e50f55
M-Vadim-dev <railer@bk.ru>
2024-12-05 01:44:40 +0300

    RA-34-implement-request-via-OkHttp-and-add-Interceptor

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -7,10 +7,13 @@ import com.example.myapp.databinding.ActivityMainBinding
 import androidx.navigation.findNavController
 import com.example.myapp.model.Category
 import kotlinx.serialization.json.Json
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import okio.IOException
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -22,63 +25,82 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val threadPool: ExecutorService = Executors.newFixedThreadPool(THREADS)
 
+    private val client: OkHttpClient by lazy {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         threadPool.execute {
-            try {
-                val connection = URL(API_CATEGORY).openConnection() as HttpURLConnection
-                connection.connect()
+            val request = Request.Builder()
+                .url(API_CATEGORY)
+                .build()
 
-                Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
-                Log.i("!!!", "responseCode: ${connection.responseCode}")
-                Log.i("!!!", "responseMessage: ${connection.responseMessage}")
-
-                val response =
-                    BufferedReader(InputStreamReader(connection.inputStream)).use { reader ->
-                        reader.readText()
-                    }
-
-                val categories: List<Category> = Json.decodeFromString(response)
-
-                val categoryIds = categories.map { it.id }
-                categoryIds.forEach { categoryId ->
-                    threadPool.execute {
+            client.newCall(request).enqueue(object : Callback {
+                override fun onResponse(call: Call, response: Response) {
+                    response.use { response ->
                         Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
-                        loadRecipesForCategory(categoryId)
+                        Log.i("!!!", "responseCode: ${response.code}")
+                        Log.i("!!!", "responseMessage: ${response.message}")
+
+                        val responseBody = response.body?.string() ?: ""
+                        val categories: List<Category> = Json.decodeFromString(responseBody)
+
+                        val categoryIds = categories.map { it.id }
+                        categoryIds.forEach { categoryId ->
+                            threadPool.execute {
+                                Log.i(
+                                    "!!!",
+                                    "Выполняю запрос на потоке: ${Thread.currentThread().name}"
+                                )
+                                loadRecipesForCategory(categoryId)
+                            }
+                        }
                     }
                 }
-            } catch (e: Exception) {
-                Log.e("!!!", "Ошибка при выполнении запроса: ${e.message}")
-            }
+
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e("!!!", "Ошибка при выполнении запроса: ${e.message}")
+                }
+            })
         }
 
-        binding.btnFavourites.setOnClickListener {
+        binding.btnFavourites.setOnClickListener() {
             findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_favorites)
         }
 
-        binding.btnCategory.setOnClickListener {
+        binding.btnCategory.setOnClickListener() {
             findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_category)
         }
 
     }
 
     private fun loadRecipesForCategory(categoryId: Int) {
-        try {
-            val connection =
-                URL("$API_CATEGORY/$categoryId/recipes").openConnection() as HttpURLConnection
-            connection.connect()
+        val request = Request.Builder()
+            .url("$API_CATEGORY/$categoryId/recipes")
+            .build()
 
-            val response =
-                BufferedReader(InputStreamReader(connection.inputStream)).use { reader ->
-                    reader.readText()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.string().let { response ->
+                    Log.i("!!!", "Рецепты для категории ID $categoryId: $response")
                 }
+            }
 
-            Log.i("!!!", "Рецепты для категории ID $categoryId: $response")
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e(
+                    "!!!",
+                    "Ошибка при получении рецептов для категории ID $categoryId: ${e.message}"
+                )
+            }
+        })
 
-        } catch (e: Exception) {
-            Log.e("!!!", "Ошибка при получении рецептов для категории ID $categoryId: ${e.message}")
-        }
     }
 }
