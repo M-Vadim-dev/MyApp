986acd34f0d431157c326571ab0879f48bfbf227
M-Vadim-dev <railer@bk.ru>
2024-12-14 01:02:36 +0300

    RA-38-Add-Jetpack-Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,8 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     id("kotlin-parcelize")
     kotlin("plugin.serialization") version "2.0.21"
-    id("androidx.navigation.safeargs.kotlin") version "2.8.4"
+    id("androidx.navigation.safeargs.kotlin") version "2.8.5"
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29"
 }
 
 android {
@@ -30,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -43,6 +44,8 @@ android {
 }
 
 dependencies {
+    ksp(libs.androidx.room.compiler.v250)
+    implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.ui)
